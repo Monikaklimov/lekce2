c832c8374dbc2ddcce7db97fa9b82966593302f3
Monika Klimova <pitroch04@gmail.com>
2024-09-30 20:13:55 +0200

    3.commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 /*/Jeden lístek do divadla Pěst na oko stojí 12 euro. Spočítejte měsíční příjem divadla ze vstupného, přichází-li průměrně 174 návštěvníků na jedno představení a divadlo hraje 15 představení měsíčně. Uložte výsledek do proměnné prijem. Hodnotu proměnné prijem vypište do stránky. Divadlo se rozhodlo prodávat studentské vstupné ve výši 65 % plného vstupného. Jak se změní měsíční příjem divadla pokud víme, že 40 % návštěvníků jsou studenti? Vypište do stránky i příjem divadla se započítanou slevou pro studenty.*/
 
 console.log("jmenuji se Monika")
+const prijem = 12 * 174 * 15
+console.log(prijem)
